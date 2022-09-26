6f44a88a8502aa44babaf55b79260593f378ca81
mesnard2u <yanis.mesnard.gaming@gmail.com>
2022-09-26 09:38:04 +0200

    Modif dwameur sursis

--- buttons/welcome/accept-button.js
+++ buttons/welcome/accept-button.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "accept-button",
     async runInteraction(client, interaction) {
-        await interaction.member.roles.add('1014460040872206447');
+        await interaction.member.roles.add('1022810813335097386');
         await interaction.member.roles.remove('1016288996546068491');
         await interaction.reply({ content: `Tu as maintenant accès au serveur ! Bienvenue dans la communauté DWAM 🥳`, ephemeral: true});
     }
